e343a93d7d17ac583ce2b088128bd135778cbaeb
qinla <2698198549@qq.com>
2022-05-18 17:14:07 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,9 +4,6 @@ public class GitTest {
     public static void main(String[] agrs){
         System.out.println("hello git!");
         System.out.println("This is my second modification.");
-        System.out.println("This is my third modification.");
-        System.out.println("好麻烦呀");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
+
     }
 }
