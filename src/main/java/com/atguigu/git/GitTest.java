db281e2d09a7b565694223b5207a893399099d97
cici520 <79502802+cici303@users.noreply.github.com>
2022-05-18 17:19:29 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] agrs){
         System.out.println("hello git!");
         System.out.println("This is my second modification.");
-        System.out.println("pull test!");
+        System.out.println("pull test heihehi!");
+        
     }
 }
