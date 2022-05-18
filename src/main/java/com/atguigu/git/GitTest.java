c4f02f6550983724c55ac13238734fad0bece335
qinla <2698198549@qq.com>
2022-05-18 16:36:18 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] agrs){
         System.out.println("hello git!");
+        System.out.println("This is my second modification.");
     }
 }
