686609ecf20bd0d507b01f03cb3767bb08f2a599
qinla <2698198549@qq.com>
2022-05-18 16:36:53 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] agrs){
         System.out.println("hello git!");
         System.out.println("This is my second modification.");
+        System.out.println("This is my third modification.");
     }
 }
