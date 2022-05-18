f5895bc8fbd0a3178292e34e0200007711227393
qinla <2698198549@qq.com>
2022-05-18 16:43:34 +0800

    hot-fix f1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("This is my second modification.");
         System.out.println("This is my third modification.");
+        System.out.println("好麻烦呀");
     }
 }
